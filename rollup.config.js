28870e4fb54d61d51121815e8f316e6b870a8317
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-04-23 03:13:25 +0300

    13.10.0

--- rollup.config.js
+++ rollup.config.js
@@ -3,22 +3,27 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 import replace from 'rollup-plugin-replace'
 import uglify from 'rollup-plugin-uglify'
 
-export default {
-  external: ['infestines', 'kefir', 'kefir.combines', 'partial.lenses'],
+const globals = {
+  infestines: 'I',
+  kefir: 'Kefir',
+  'kefir.combines': 'kefir.combines',
+  'partial.lenses': 'partial.lenses'
+}
+
+const build = ({NODE_ENV, format, suffix}) => ({
+  external: Object.keys(globals),
+  input: 'src/kefir.partial.lenses.js',
   output: {
-    globals: {
-      infestines: 'I',
-      kefir: 'Kefir',
-      'kefir.combines': 'kefir.combines',
-      'partial.lenses': 'partial.lenses'
-    }
+    globals,
+    name: 'kefir.partial.lenses',
+    format,
+    file: `dist/kefir.partial.lenses.${suffix}`
   },
   plugins: [
-    process.env.NODE_ENV &&
-      replace({'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)}),
+    NODE_ENV && replace({'process.env.NODE_ENV': JSON.stringify(NODE_ENV)}),
     nodeResolve(),
     babel(),
-    process.env.NODE_ENV === 'production' &&
+    NODE_ENV === 'production' &&
       uglify({
         compress: {
           hoist_funs: true,
@@ -28,4 +33,11 @@ export default {
         }
       })
   ].filter(x => x)
-}
+})
+
+export default [
+  build({format: 'cjs', suffix: 'cjs.js'}),
+  build({format: 'es', suffix: 'es.js'}),
+  build({format: 'umd', suffix: 'js', NODE_ENV: 'dev'}),
+  build({format: 'umd', suffix: 'min.js', NODE_ENV: 'production'})
+]
